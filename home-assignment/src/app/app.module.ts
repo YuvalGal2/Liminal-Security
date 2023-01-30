ec7ce44e921999fad2843b370b39782f1b31a8b7
Yuval Gal <yuvalgal017@gmail.com>
2023-01-30 20:32:08 +0200

    moved everything to external module

--- home-assignment/src/app/app.module.ts
+++ home-assignment/src/app/app.module.ts
@@ -1,46 +1,24 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TasksListComponent } from './tasks-list/tasks-list.component';
-import { ListItemComponent } from './tasks-list/list-item/list-item.component';
-import { ListMessageComponent } from './tasks-list/list-message/list-message.component';
 import {HttpClientModule} from "@angular/common/http";
-import { ListItemControlComponent } from './tasks-list/list-item/list-item-control/list-item-control.component';
-import { FiltersComponent } from './tasks-list/filters/filters.component';
-import { SearchComponent } from './tasks-list/filters/search/search.component';
-import { ToggleComponent } from './tasks-list/filters/toggle/toggle.component';
+import {TasksModule} from "./modules/task-list.module";
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatSlideToggleModule} from "@angular/material/slide-toggle";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { ItemFormComponent } from './item-form/item-form.component';
-import { AddItemButtonComponent } from './tasks-list/add-item-button/add-item-button.component';
-import {MatSelectModule} from '@angular/material/select';
 @NgModule({
   declarations: [
     AppComponent,
-    TasksListComponent,
-    ListItemComponent,
-    ListMessageComponent,
-    ListItemControlComponent,
-    FiltersComponent,
-    SearchComponent,
-    ToggleComponent,
-    ItemFormComponent,
-    AddItemButtonComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FormsModule,
-    MatSlideToggleModule,
-    ReactiveFormsModule,
-    MatSelectModule
+    TasksModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
--- home-assignment/src/app/modules/task-list.module.ts
+++ home-assignment/src/app/modules/task-list.module.ts
@@ -1,1 +1,52 @@
-
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from '../app.component';
+import { TasksListComponent } from '../tasks-list/tasks-list.component';
+import { ListItemComponent } from '../tasks-list/list-item/list-item.component';
+import { ListMessageComponent } from '../tasks-list/list-message/list-message.component';
+import {HttpClientModule} from "@angular/common/http";
+import { ListItemControlComponent } from '../tasks-list/list-item/list-item-control/list-item-control.component';
+import { FiltersComponent } from '../tasks-list/filters/filters.component';
+import { SearchComponent } from '../tasks-list/filters/search/search.component';
+import { ToggleComponent } from '../tasks-list/filters/toggle/toggle.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { ItemFormComponent } from '../item-form/item-form.component';
+import { AddItemButtonComponent } from '../tasks-list/add-item-button/add-item-button.component';
+import {MatSelectModule} from '@angular/material/select';
+import {TasksService} from "../services/tasks.service";
+@NgModule({
+  declarations: [
+    TasksListComponent,
+    ListItemComponent,
+    ListMessageComponent,
+    ListItemControlComponent,
+    FiltersComponent,
+    SearchComponent,
+    ToggleComponent,
+    ItemFormComponent,
+    AddItemButtonComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule,
+    MatSelectModule
+  ],
+  providers: [TasksService],
+  bootstrap: [AppComponent],
+  exports: [ TasksListComponent,
+    ListItemComponent,
+    ListMessageComponent,
+    ListItemControlComponent,
+    FiltersComponent,
+    SearchComponent,
+    ToggleComponent,
+    ItemFormComponent,
+    AddItemButtonComponent]
+})
+export class TasksModule { }
